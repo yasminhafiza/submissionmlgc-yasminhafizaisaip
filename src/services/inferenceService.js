73a3e66f3e6d9acab1225c6421f6b13ce77885e5
yasminhafiza <154599009+yasminhafiza@users.noreply.github.com>
2024-05-21 12:26:53 +0700

    inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -1,27 +1,32 @@
-
 const tf = require('@tensorflow/tfjs-node');
 const InputError = require('../exceptions/InputError');
 
 async function predictClassification(model, image) {
     try {
         const tensor = tf.node
-            .decodeImage(image)
+            .decodeJpeg(image)
             .resizeNearestNeighbor([224, 224])
             .expandDims()
-            .toFloat()
+            .toFloat();
 
         const prediction = model.predict(tensor);
         const score = await prediction.data();
-        const confidenceScore = score[0] * 100;
+        const confidenceScore = Math.max(...score) * 100;
 
-        const classes = ['Non-cancer', 'Cancer'];
-        const label = confidenceScore > 50 ? 'Cancer' : 'Non-cancer';
+        let result, suggestion;
+        
+        if (confidenceScore > 50) {
+            result = "Cancer";
+            suggestion = "Segera periksa ke dokter!";
+        }else {
+            result = "Non-cancer";
+            suggestion = "Anda sehat!";
+        }
 
-        const suggestion = label === 'Cancer' ? 'Segera periksa ke dokter!' : 'Anda sehat!';
+        return { result, suggestion };
 
-        return { label, suggestion, confidenceScore }
     } catch (error) {
-        throw new InputError(`Terjadi kesalahan input: ${error.message}`)
+        throw new InputError(`Terjadi kesalahan input: ${error.message}`);
     }
 }
 
