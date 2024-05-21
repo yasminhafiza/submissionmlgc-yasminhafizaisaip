f5846a1ce425dbcd09c147f32753d225e670ac85
yasminhafiza <154599009+yasminhafiza@users.noreply.github.com>
2024-05-21 13:18:28 +0700

    storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,14 +1,10 @@
-
 const { Firestore } = require('@google-cloud/firestore');
-
+ 
 async function storeData(id, data) {
-    const db = new Firestore({
-        projectId: 'submissionmlgc-yasminhafizaip',
-        keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
-    });
-
-    const predictCollection = db.collection('predictions');
-    return predictCollection.doc(id).set(data);
+  const db = new Firestore('cancer');
+ 
+  const predictCollection = db.collection('prediction');
+  return predictCollection.doc(id).set(data);
 }
-
+ 
 module.exports = storeData;
