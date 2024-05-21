839bbe8bde1f27671db42aca82a38038fdd25973
yasminhafiza <154599009+yasminhafiza@users.noreply.github.com>
2024-05-21 12:26:27 +0700

    server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,54 +1,53 @@
 require('dotenv').config();
-
+ 
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
-
-(async() => {
+ 
+(async () => {
     const server = Hapi.server({
         port: 3000,
         host: '0.0.0.0',
         routes: {
             cors: {
-                origin: ['*'],
+              origin: ['*'],
             },
-            payload: {
-                maxBytes: 1000000
-            }
         },
     });
-
+ 
     const model = await loadModel();
     server.app.model = model;
-
+ 
     server.route(routes);
-
-    server.ext('onPreResponse', function(request, h) {
+ 
+    server.ext('onPreResponse', function (request, h) {
         const response = request.response;
-
-        if (response instanceof InputError) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: `${response.message} Silahkan gunakan foto lain.`
-            })
-            newResponse.code(response.statusCode)
-            return newResponse;
-        }
 
         if (response.isBoom && response.output.statusCode === 413) {
             const newResponse = h.response({
                 status: 'fail',
-                message: 'Payload content length greater than maximum allowed: 1000000'
+                message: 'Payload content length greater than maximum allowed: 1000000',
             });
+            
             newResponse.code(413);
             return newResponse;
         }
 
+        if (response instanceof InputError || response.isBoom) {
+            const statusCode = response instanceof InputError ? response.statusCode : response.output.statusCode;
+            const newResponse = h.response({
+                status: 'fail',
+                message: 'Terjadi kesalahan dalam melakukan prediksi',
+            });
 
+            newResponse.code(parseInt(statusCode));
+            return newResponse;
+        }
+ 
         return h.continue;
     });
-
+ 
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
